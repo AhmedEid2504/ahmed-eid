a2f4ce65643b43a6accf77c9d5190eb1ac75d5dc
Ahmed Eid <ahmedeid2504@gmail.com>
2024-07-19 17:05:33 +0300

    change text color and z index of spotlight

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ import {  FaArrowDown } from 'react-icons/fa';
 const Hero = () => {
     return (
         <div>
-            <div>
+            <div className='-z-10'>
                 <Spotlight 
                     className='max-sm:top-[21dvh] sm:top-[12dvh] md:top-[5dvh] -right-10 h-screen w-[100vw]'
                     fill='#00fff9'
@@ -26,7 +26,7 @@ const Hero = () => {
             <div className="h-screen w-screen relative dark:bg-black-100 bg-white  dark:bg-grid-white/[0.04] bg-grid-black/[0.01] flex items-center justify-center">
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
             </div>
-            <div className='absolute w-screen h-screen top-0 left-0 flex flex-col justify-center items-center'>
+            <div className='z-50 absolute w-screen h-screen top-0 left-0 flex flex-col justify-center items-center'>
                 <div className='max-w-[89vw]'>
                     <TextGenerateEffect 
                         words='Hi, I&apos;m Ahmed Eid and I&apos;m a' 
@@ -37,7 +37,7 @@ const Hero = () => {
                     />
                     <TextGenerateEffect 
                         words='<ReactFrontEndDeveloper'
-                        textColor='text-clr_1'
+                        textColor='text-clr_2'
                         delay={0.5}
                         duration={5}
                         textFamily='font-serif'
@@ -45,7 +45,7 @@ const Hero = () => {
                     />
                     <TextGenerateEffect 
                         words="talented = ' true '"
-                        textColor='text-clr_1'
+                        textColor='text-clr_2'
                         delay={0.5}
                         duration={8}
                         textFamily='font-serif'
@@ -53,7 +53,7 @@ const Hero = () => {
                     />
                     <TextGenerateEffect 
                         words='/>'
-                        textColor='text-clr_1'
+                        textColor='text-clr_2'
                         delay={0.5}
                         duration={5}
                         textFamily='font-serif'
